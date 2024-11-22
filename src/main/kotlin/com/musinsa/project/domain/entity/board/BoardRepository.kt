60f8e6481062749798fadcd97a7b45bc0ab72f53
tow <tahho22@gmail.com>
2024-11-22 16:17:45 +0900

    [구현 4] Domain 추가

--- src/main/kotlin/com/musinsa/project/domain/entity/board/BoardRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.musinsa.project.domain.entity.board
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BoardRepository : JpaRepository<Board, Long>
---
+++ src/main/kotlin/com/musinsa/project/domain/entity/brand/Brand.kt
@@ -0,0 +1,48 @@
+package com.musinsa.project.domain.entity.brand
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.DynamicUpdate
+import java.time.Instant
+
+@Entity
+@DynamicUpdate
+@Table(name = "brand")
+class Brand private constructor(
+    val createdAt: Instant = Instant.now(),
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    var brandName: String = ""
+    var deleted: Boolean = false
+    var updatedAt: Instant? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Brand
+
+        if (createdAt != other.createdAt) return false
+        if (id != other.id) return false
+        if (brandName != other.brandName) return false
+        if (deleted != other.deleted) return false
+        if (updatedAt != other.updatedAt) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = createdAt.hashCode()
+        result = 31 * result + (id?.hashCode() ?: 0)
+        result = 31 * result + brandName.hashCode()
+        result = 31 * result + deleted.hashCode()
+        result = 31 * result + (updatedAt?.hashCode() ?: 0)
+        return result
+    }
+}
