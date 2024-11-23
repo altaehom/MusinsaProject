c90a6637c2fbc282cab1aa8cc91c6bf287e55ca3
tow <tahho22@gmail.com>
2024-11-23 18:34:26 +0900

    [구현 4] Brand API 구현

---
+++ src/main/kotlin/com/musinsa/project/domain/service/brand/model/BrandModel.kt
@@ -0,0 +1,23 @@
+package com.musinsa.project.domain.service.brand.model
+
+import com.musinsa.project.domain.entity.brand.Brand
+import java.time.Instant
+
+data class BrandModel private constructor(
+    val id: Long,
+    val brandName: String,
+    val createAt: Instant,
+    val updateAt: Instant?,
+) {
+    companion object {
+        operator fun invoke(item: Brand): BrandModel =
+            with(item) {
+                BrandModel(
+                    id = id!!,
+                    brandName = brandName,
+                    createAt = createdAt,
+                    updateAt = updatedAt,
+                )
+            }
+    }
+}
