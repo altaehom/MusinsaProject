3eeb72ef99b91ad9e71049509e0075075d19c2f8
tow <tahho22@gmail.com>
2024-11-26 17:38:38 +0900

    [구현 3] API 추가

--- src/main/kotlin/com/musinsa/project/application/ranking/model/CategoryPriceRankModel.kt
+++ src/main/kotlin/com/musinsa/project/application/ranking/model/CategoryPriceRankModel.kt
@@ -2,7 +2,7 @@ package com.musinsa.project.application.ranking.model
 
 import java.math.BigDecimal
 
-data class LowestCategoryPriceRankModel(
+data class CategoryPriceRankModel(
     val categoryId: Long,
     val brandId: Long?,
     val price: BigDecimal?,
